3b22e80c72942cff1296859f3b22fba891845070
Suhrid Mulay <suhridmulay@pop-os.localdomain>
2021-04-06 19:28:13 +0530

    Added barebones router and network classes

--- dvr.py
+++ dvr.py
@@ -12,13 +12,24 @@ class Router:
       # Entry format {dest: fwd_to}
       self.routing_table: dict = dict()
 
+   # Creates a one way connection entry in router
+   def connect(self, r: 'Router', wt: float):
+      if (r in self.adjacents):
+         print('[WARNING] router already connected, updating link weight')
+         self.distance_vector[r.id] = wt
+      else:
+         self.adjacents.append(r)
+         self.routing_table[r.id] = r
+         self.distance_vector[r.id] = wt
+
+
 
 
 class Network:
    def __init__(self, network_name: str):
-      self.name = network_name
-      self.router_list = list()
-      self.size = 0
+      self.name: str = network_name
+      self.router_list: list = list()
+      self.size: int = 0
 
    def add_router(self, rt: 'Router'):
       if rt in self.router_list:
